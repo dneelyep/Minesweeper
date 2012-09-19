e8ace829125406a19bbdac8521899eed003387ee
Daniel Neel <dneelyep@gmail.com>
2012-09-18 21:51:50 -0400

    Added some basic code to initialize the GUI.

--- src/com/minesweeper/Main.java
+++ src/com/minesweeper/Main.java
@@ -1,7 +1,25 @@
 package com.minesweeper;
 
-class Main {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/** Main frame that houses the rest of the visual components for the game. */
+class Main extends JFrame {
+
+    /** Main method. */
     public static void main(String[] args) {
-	System.out.println("Testing out my program.");
+	new Main();
     }
+
+    /** Create a new JFrame. */
+    public Main() {
+	super("Minesweeper");
+
+	this.add(new JLabel("A test label, yo!"));
+	this.pack();
+	this.setVisible(true);
+
+	for (int i = 0; i < 10000000; i++) {
+	}
+    }	    
 }
